cd88e65c29f5ca2220ef1c8116cb4642cad2c936
Taras <doxa2006@ya.ru>
2014-07-15 12:35:41 +0400

    Add comments
    Add comments to use

--- list.php
+++ list.php
@@ -3,6 +3,12 @@
 List snippet by Doxa
 
 Use [[List? &tv=`elem 1||elem 2||elem 3..`]] and get ul-li list
+
+Params: 
+&tv - list from tv
+&outerTpl - outer element, default <ul>[+list+]</ul>. When change outer element use [+list+] to detect list position.
+&rowTpl - list element, default <li>[+text+]</li>. When change outer element use [+text+] to detect text position.
+&separator - default ||. 
 ***/
 
 $tv = isset( $tv ) ? $tv : NULL;
